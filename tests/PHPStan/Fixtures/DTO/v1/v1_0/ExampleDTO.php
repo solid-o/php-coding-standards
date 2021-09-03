9dd77a852651e11bdf24d2fa7acf9d7d5f44c72e
Alessandro Chitolina <alekitto@gmail.com>
2021-09-03 11:23:31 +0200

    add dto resolver dynamic type extension for phpstan

---
+++ tests/PHPStan/Fixtures/DTO/v1/v1_0/ExampleDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\PHPStan\Fixtures\DTO\v1\v1_0;
+
+use Tests\PHPStan\Fixtures\DTO\Contracts\ExampleDTOInterface;
+
+class ExampleDTO implements ExampleDTOInterface
+{
+}
---
+++ tests/PHPStan/Fixtures/DTO/v1/v1_0/NonDTO.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PHPStan\Fixtures\DTO\v1\v1_0;
+
+class NonDTO
+{
+}
